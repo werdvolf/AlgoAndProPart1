af3fb0977854e0b2bc6fe5d16a0b8639387a87d2
werdvolf <werdvolf@gmail.com>
2018-11-29 13:51:18 +0200

    Update Lab3.c

--- Lab-3/Lab3.c
+++ Lab-3/Lab3.c
@@ -1,8 +1,39 @@
-//
-//  Lab3.c
-//  
-//
-//  Created by Slavik on 11/29/18.
-//
+#include <stdio.h>
+#include <math.h>
 
-#include "Lab3.h"
+
+int main(void)
+{
+    float     a = 0.1;
+    float     b = 0.8;
+    float     e = 0.0001;
+    int       n = 3;
+
+
+    for(float x=a; x<=b; x+=(b-a)/10)
+    {
+        float y, sumn = 0, sume = 0;
+
+        y = (0.25*logf((1+x)/(1-x))+0.5*atan(x))-x;
+
+        for(int j=1; j<=n; j++)
+        {
+            sumn += pow(x,(4*j+1))/(4*j+1);
+        }
+
+        int counter = 0;
+        float inc = 0;
+
+        do
+        {
+        	counter++;
+            inc = pow(x,(4*counter+1))/(4*counter+1);
+            sume += inc;
+        }
+        while(inc>=e);
+
+        printf("X=%.12f\tSumN=%.12f\tSumE=%.12f\tY=%.12f\n", x, sumn, sume, y);
+    }
+
+}
+
